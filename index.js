9fe5d01a3f27110a4bc44e245e73f8a6495e2ae2
Brandon Royal <bmroyal@gmail.com>
2022-07-19 15:00:17 +0000

    change

--- index.js
+++ index.js
@@ -24,5 +24,5 @@ app.get('/', (req, res) => {
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
-  console.log('Hello world listening on port', port);
+  console.log('Hello world2 listening on port', port);
 });

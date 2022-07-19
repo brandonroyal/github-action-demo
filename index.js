44b6f10f03986faddeddea772e756514232c4820
Brandon Royal <bmroyal@gmail.com>
2022-07-19 17:59:03 +0000

    another change

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  console.log('Hello world received a request.');
+  console.log('Hello Chicago received a request.');
 
   const target = process.env.TARGET || 'World';
   res.send(`Hello ${target}!`);
